e9b25bf3fe404a8f5543fafc66f2c34c73d93212
servet-isikli <servetisikli@yahoo.com>
2023-06-10 01:42:49 +0200

    day04-L-3-2 edited

--- solutions/solutions/src/solutions/Day04L32.js
+++ solutions/solutions/src/solutions/Day04L32.js
@@ -1,10 +1,41 @@
 import React from "react";
 import logoImage from "../solutions-assets/logo-image.png";
+import "./Day04L32.css";
 
 const Day04L32 = () => {
   return (
     <div>
-      <img src={logoImage} alt="logoImage" />
+      <img className="logo" src={logoImage} alt="logoImage" />
+      <h3 className="logoo">SOLFA ART</h3>
+      <p>Digital Solutions</p>
+      <h4>SKILLS</h4>
+
+      <div className="skills">
+        <p>HTML</p>
+        <p>css</p>
+        <p>Sass</p>
+        <p>JS</p>
+        <p>React</p>
+        <p>Redux</p>
+        <p>Node</p>
+        <p>MongoDB</p>
+        <p>Python</p>
+        <p>Flask</p>
+        <p>Django</p>
+        <p>NumPy</p>
+        <p>Pandas</p>
+        <p>Data Analysis</p>
+        <p>MYSQL</p>
+        <p>GraphQL</p>
+        <p>D3.js</p>
+        <p>Gatsby</p>
+        <p>Docker</p>
+        <p>Heroku</p>
+        <p>Git</p>
+      </div>
+      <div>
+        <p>Joined on June 30, 2023</p>
+      </div>
     </div>
   );
 };
