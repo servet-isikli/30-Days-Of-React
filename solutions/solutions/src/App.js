4e3c5abfad4af39d60a99dba86d50ce218e2e340
servet-isikli <servetisikli@yahoo.com>
2023-06-09 18:33:34 +0200

    app.js uptaded

--- solutions/solutions/src/App.js
+++ solutions/solutions/src/App.js
@@ -2,6 +2,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Day04L21 from "./solutions/Day04L21";
 
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
     <div className="App">
       <Header />
       <Main />
-      <Footer/>
+      <Footer />
+      <h2>04-Day: Exercises: Components - Solutions</h2>
+      <h3>04-Day:1</h3>
+      <Day04L21 />
     </div>
   );
 }
