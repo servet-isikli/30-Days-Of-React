47c2750becf022eec2588a5b7180c0753deb9194
servet-isikli <servetisikli@yahoo.com>
2023-06-09 03:00:32 +0200

    Header is created

---
+++ solutions/solutions/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+
+const headerStyles = {
+  backgroundColor: "blue",
+  color: "white",
+  padding: "10px",
+};
+
+const Header = () => {
+  return (
+    <header style={headerStyles}>
+      <div className="header-wrapper">
+        <h1>Welcome to 30 Days Of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>JavaScript Library</h3>
+        <p>S.I.</p>
+        <small>June 9, 2023</small>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
